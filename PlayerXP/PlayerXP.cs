996a42983a9a11d38b457979d6e8f3f8ec7e849b
Todd <infernogaming621@gmail.com>
2018-11-13 20:43:14 -0500

    verison change again

--- PlayerXP/PlayerXP.cs
+++ PlayerXP/PlayerXP.cs
@@ -12,7 +12,7 @@ namespace PlayerXP
 	name = "Player XP",
 	description = "A plugin that lets players collect XP and level up.",
 	id = "cyan.playerxp",
-	version = "0.4",
+	version = "0.5",
 	SmodMajor = 3,
 	SmodMinor = 0,
 	SmodRevision = 0
